cfc457801ab4a9de8aab9ea018d2868a6a012041
khalilGame469 <92345413+khalilGame469@users.noreply.github.com>
2021-11-15 21:19:33 -0500

    Fixed Level Issues
    Here You go

--- Project_R4T_Capstone/Assets/Scripts/Shooting.cs
+++ Project_R4T_Capstone/Assets/Scripts/Shooting.cs
@@ -23,7 +23,7 @@ public class Shooting : MonoBehaviour
     {
         Vector2 lookDir = mousePos - rb.position;
 
-        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 0f;
         rb.rotation = angle;
     }
 
