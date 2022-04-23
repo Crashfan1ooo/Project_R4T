c07337f04dbc534afeec57b12fa03a8ce78a5478
khalilGame469 <92345413+khalilGame469@users.noreply.github.com>
2022-04-23 09:43:59 -0400

    PostP Time Slow Effect
    Added effects to the time slow

--- Project_R4T_Capstone/Assets/Scripts/PlayerScripts/PostPEffects.cs
+++ Project_R4T_Capstone/Assets/Scripts/PlayerScripts/PostPEffects.cs
@@ -3,24 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
 
-public class PostProcessingEffects : MonoBehaviour
+public class PostPEffects : MonoBehaviour
 {
     [SerializeField] public SlowGauge timeDrain; //this variable is to connect the the gauge and allow the leech to drain
     private PostProcessVolume _postProcessVolume; //call the Post-process Volume that has effects
-    private ChromaticAberration _cA;
+    public ChromaticAberration _cA;
     private Bloom _bL;
     private Grain _gR;
 
-    
+
 
     // Start is called before the first frame update
     void Start()
     {
-
         timeDrain = GameObject.Find("Canvas").transform.GetChild(0).GetComponent<SlowGauge>();
-        _postProcessVolume = GameObject.Find("Canvas").GetComponent<PostProcessVolume>();
-       
-
+        _postProcessVolume = GameObject.Find("PostProcessProfile").GetComponent<PostProcessVolume>();
+        _postProcessVolume.profile.TryGetSettings(out _cA);
+        _postProcessVolume.profile.TryGetSettings(out _bL);
+        _postProcessVolume.profile.TryGetSettings(out _gR);
     }
 
     private void Update()
@@ -35,28 +35,29 @@ public class PostProcessingEffects : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Mouse1) && timeDrain.currentSlow > 1)
         {
             _cA.active = true;
-            
+            _cA.SetAllOverridesTo(true);
         }
         if (Input.GetKeyUp(KeyCode.Mouse1) || timeDrain.currentSlow <= 1)
         {
-            
-           // _cA.SetAllOverridesTo(false);
+
+            _cA.SetAllOverridesTo(false);
         }
 
 
-    } 
+    }
 
     public void Bloom()
     {
         if (Input.GetKeyDown(KeyCode.Mouse1) && timeDrain.currentSlow > 1)
         {
-            _bL.active = true ;
+            _bL.active = true;
+            _bL.SetAllOverridesTo(true);
 
         }
         if (Input.GetKeyUp(KeyCode.Mouse1) || timeDrain.currentSlow <= 1)
         {
 
-           // _bL.SetAllOverridesTo(false);
+             _bL.SetAllOverridesTo(false);
         }
     }
 
@@ -65,14 +66,16 @@ public class PostProcessingEffects : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Mouse1) && timeDrain.currentSlow > 1)
         {
             _gR.active = true;
+            _gR.SetAllOverridesTo(true);
 
         }
         if (Input.GetKeyUp(KeyCode.Mouse1) || timeDrain.currentSlow <= 1)
         {
 
-          //  _gR.SetAllOverridesTo(false);
+              _gR.SetAllOverridesTo(false);
         }
     }
+
 
 
 }
