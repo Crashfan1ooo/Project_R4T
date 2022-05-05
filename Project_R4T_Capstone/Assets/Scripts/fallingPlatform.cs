8bb52ae88cb56c9a698c3a39eb4500efdc453f6f
ShockingBlaze134 <71789038+ShockingBlaze134@users.noreply.github.com>
2022-05-05 13:34:11 -0700

    Falling Platform added

---
+++ Project_R4T_Capstone/Assets/Scripts/fallingPlatform.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fallingPlatform : MonoBehaviour
+{
+    bool isFalling = false;
+    float downSpeed = 0;
+
+
+     void OnTriggerEnter(Collider collider)
+    {
+        if(collider.gameObject.CompareTag("Player"))
+        {
+            isFalling = true;
+            Destroy(gameObject, 10);
+        }
+    }
+
+     void Update()
+    {
+        if(isFalling)
+        {
+            downSpeed += Time.deltaTime/10;
+            transform.position = new Vector3(transform.position.x,
+                transform.position.y - downSpeed,
+                transform.position.z);
+        }
+    }
+
+
+}
